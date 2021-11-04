a7c9272df878b8dcb107a4924f65c2822f837344
Khuyen Nguyen <khuyennguyen@Khuyens-Air.lan>
2021-11-03 19:43:26 -0700

    all created functions

---
+++ Geometry.R
@@ -0,0 +1,23 @@
+circleArea<-function(r){
+  return (pi*r*r)
+}
+
+squareArea<-function(side){
+  return (side*side)
+}
+
+cubeVolume<-function(L, W, H){
+  return (L*W*H)
+}
+
+celcius_to_Kelvin<-function(C){
+  return (C+273.15)
+}
+
+print_hello<-function(){
+  print ("hello world")
+}
+
+isEven<-function(n){
+  return (n%%2 == 0)
+}
